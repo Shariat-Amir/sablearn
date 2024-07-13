d3de4cc0bdcc5f917865f8ac61137108a64cc72c
Shariat-Amir <shariat.amir.1997@gmail.com>
2024-07-13 02:03:45 -0700

    starting project

---
+++ core/allCourses.js
@@ -0,0 +1,113 @@
+ const allCourses=[
+    {
+        id:1,
+        title:"آموزش جنگو به صورت پروژه محور",
+        disc:"از ویژگی های مهم دوره آموزش جنگو میتونیم به توضیح کامل مفاهیم ساختاری و...",
+        teacher:"رضا دولتی",
+        studentsNum:716,
+        price:3500000,
+        imgSrc:'/images/courses/django.webp'
+    },
+    {
+        id:2,
+        title:"مینی پروژه های تخصصی با Css ,Html+دیزاین اختصاصی",
+        disc:"عنوان دوره گویای همه چی هست اما نیازه برخی موارد گفته بشه تا بتونید با دید بهتری ",
+        teacher:"حمیدرضا عبادی",
+        studentsNum:551,
+        price:720000,
+        imgSrc:'/images/courses/HTML&CSS.webp'
+    },
+    {
+        id:3,
+        title:"آموزش جامع api نویسی با PHP",
+        disc:"API  یکی از پرکاربرد ترین اجزای نرم افزاریه که حتما باید بلد باشی ! توی این دوره قراره api",
+        teacher:"معین باغشیخی",
+        studentsNum:65,
+        price:950000,
+        imgSrc:'/images/courses/PHP.webp'
+    },
+    {
+        id:4,
+        title:"آموزش جامع webpack",
+        disc:"فرقی نمی کند که شما یک برنامه نویس تازه کار ویا حرفه ای باشید ، در هرصورت به وب پک نیاز دارید",
+        teacher:"آرمین هوشمند",
+        studentsNum:110,
+        price:690000,
+        imgSrc:'/images/courses/webpack.webp'
+    },
+    {
+        id:5,
+        title:"آموزش پرژه محور WebSocket",
+        disc:"با Socket میتونی مثل تلگرام یا سایت های ارز دیجیتال یک ارتباط Realtime بسازید.",
+        teacher:"محمد امین سعیدی راد",
+        studentsNum:715,
+        price:1190000,
+        imgSrc:'/images/courses/WebSocket.webp'
+    },
+    {
+        id:6,
+        title:"آموزش جامع پروژه محور GrapghQL",
+        disc:"واسه حرفه ای تر شدن و استخدام تو شرکت های بزرگ با حقوق بالا باید GrapghQl بلد باشی ",
+        teacher:"محمد امین سعیدی راد",
+        studentsNum:584,
+        price:970000,
+        imgSrc:'/images/courses/GrapghQL.webp'
+    },
+    {
+        id:7,
+        title:"توسعه افزونه مرورگر با جاوااسکریپت",
+        disc:"ما در این دوره ، توسعه افزونه های مرورگر رو با پیاده سازی پروزه های کاربردی با دانش فرانت ",
+        teacher:"مهرشاد براتی",
+        studentsNum:108,
+        price:950000,
+        imgSrc:'/images/courses/JS.webp'
+    },
+    {
+        id:8,
+        title:"دوره پروژه محور لایووایر(LiveWire Master)",
+        disc:"دوره لایو وایر مستر (LiveWire Master) یک دوره ی تمتم عیار برای فول استک هاست",
+        teacher:"آرمین هوشمند",
+        studentsNum:44,
+        price:699000,
+        imgSrc:'/images/courses//LiveWire.webp'
+    },
+    {
+        id:9,
+        title:"تست نفوذ با BadUSB",
+        disc:"در این دوره کاربردی ما با مفهوم HID ATTACK و چگونگی پیاده سازی سناروی های جذاب از...",
+        teacher:"اشکان مقدس",
+        studentsNum:60,
+        price:1300000,
+        imgSrc:'/images/courses/HIdAttack.webp'
+    },
+    {
+        id:10,
+        title:"Data Visualization با پایتون",
+        disc:"ما توی دنیایی زندگی میکنیم که همه چیز به سمت سریع تر شدن حرکت میکنه . برای ذهن...",
+        teacher:"رضا دولتی",
+        studentsNum:332,
+        price:619500,
+        imgSrc:'/images/courses/dataVisualiazationWithPython.webp'
+    },
+    {
+        id:11,
+        title:"توسعه کتابخانه با جاوا اسکریپت",
+        disc:"توسعه کتابخانه ، نمونه کار قوی برای رزومه شماست و این دوره آموزش یادگیری چنین...",
+        teacher:"مهرشاد براتی",
+        studentsNum:81,
+        price:1300000,
+        imgSrc:'/images/courses/jsLibrary.webp'
+    },
+    {
+        id:12,
+        title:"بهینه نویسی کد ها در پایتون",
+        disc:"همه میدونیم که زبان برنامه نویسی پایتون یه زبان خیلی جذاب ، راحت و همینطور کاربردیه...",
+        teacher:"رضا دولتی",
+        studentsNum:753,
+        price:540000,
+        imgSrc:'/images/courses//pythonOptimization.webp'
+    },
+]
+
+
+export default allCourses

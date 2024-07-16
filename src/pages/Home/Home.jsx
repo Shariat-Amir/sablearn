38ae3f86ca99c4f3cc9140cd4bf4d658e0b6af82
Shariat-Amir <shariat.amir.1997@gmail.com>
2024-07-16 09:18:42 -0700

    warning animation set

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import  { useEffect, useRef, useState } from 'react';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { Hero } from '../../features';
+import './Home.css'
 
 function Notification() {
     const [showDiv, setShowDiv] = useState(true);
@@ -24,10 +25,10 @@ function Notification() {
       <>
       <Hero />
        {showDiv && (
-            <div ref={ref} className="dark:bg-slate-600 w-[700px] h-[200px] gap-5 px-5 rounded-lg shadow-lg flex flex-col justify-center items-center text-center z-10 absolute top-40 left-[30%]">
+            <div ref={ref} className="dark:bg-slate-600 dropdown w-[700px] h-[200px] gap-5 px-5 rounded-lg shadow-lg flex flex-col justify-center items-center text-center z-10 absolute top-40 left-[30%] ">
               <span className='absolute top-2 right-2 cursor-pointer hover:text-slate-400 ' onClick={()=>{setShowDiv(false)}}><CloseOutlinedIcon /></span>
                 <p className="text-3xl text-red-400">توجه ... !</p>
-                <p className="dark:text-white">این وب سایت یک وبسایت تمرینی و الگو گرفته از سایت سبزلرن می باشد و به همین دلیل تمامی لینک های آن غیر فعال می باشد</p>
+                <p className="dark:text-white">این وب سایت یک وبسایت تمرینی و الگو گرفته از سایت سبزلرن می باشد و به همین دلیل تمامی لینک های مربوط به کلاس ها و دوره ها در آن غیر فعال می باشد</p>
             </div>
         )}
         </>
